b29b70aadf271e082f78446ecb756f431af28d33
LuanAlves17 <luan.alves@copasul.coop.br>
2025-07-06 00:40:02 -0400

    Mudando o metodo do avaliar para Put

--- src/main/java/com/cooperawiki/Wiki/controllers/RateController.java
+++ src/main/java/com/cooperawiki/Wiki/controllers/RateController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class RateController {
     @Autowired private RelevanceService relevanceService;
     
-    @PostMapping("/{targetId}")
+    @PutMapping("/{targetId}")
     public ResponseEntity<Relevance> rate(@PathVariable UUID targetId, @RequestBody RelevanceInputDto dto) throws Exception {
         return ResponseEntity.ok().body(relevanceService.rate(targetId, dto));
     }
